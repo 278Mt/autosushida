6a6ee303d8e09015a8c0148950dbc38dacbffb08
Nozomi Toba <40573499+278Mt@users.noreply.github.com>
2019-08-03 20:06:38 +0900

    もっと速く加速したくはないか

--- autosushida.py
+++ autosushida.py
@@ -7,25 +7,31 @@ automatically resolving sushida version 2.0
 @author: 278mt
 """
 
-import re
-import io
+from re import sub as resub
+from io import BytesIO
+from traceback import format_exc
+#from time import sleep, time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from PIL import Image
-import pyocr
-import pyocr.builders
+from PIL.Image import open as image_open
+from PIL.Image import fromarray
+from pyocr import get_available_tools
+from pyocr.builders import TextBuilder
+from numpy import asarray
+
 
 class AutoSushida(object):
 
     def __init__(self, level=2, input_sth=True):
         # if input_sth mode is false, input sth is off set
-        self.input_sth = input_sth
+        self.__input_sth = input_sth
 
         # alias
         course_index = level
 
         # pyocr's tool
-        self.tool = pyocr.get_available_tools()[0]
+        self.__image_to_string = get_available_tools()[0].image_to_string
+        self.__image_to_string.__defaults__ = ('eng', TextBuilder())
 
         # easy, normal, hard courses
         courses = {0:{"coord" : -60,
@@ -39,10 +45,7 @@ class AutoSushida(object):
                       "costs" : [240, 380, 380, 380, 500, 500]}}
 
         # set technical avoidant speed and coodination for identification of cost of dish flowing
-        self.y = 256
-
-        # filename of sampling png
-        self.fname = "./sample.png"
+        self.__y = 256
 
         try:
             course = courses[int(course_index)]
@@ -54,195 +57,171 @@ class AutoSushida(object):
             print("START HARD MODE")
             course = courses[2]
 
-        self.coord = course["coord"]
-        self.xs    = course["xs"]
-        self.costs = course["costs"]
+        self.__coord = course["coord"]
+        self.__xs    = course["xs"]
+        self.__costs = course["costs"]
 
-        # input technical avoidant speed
-        self.max_dishes = 700-1
 
-    def _open_chrome(self):
+    def __open_chrome(self):
         # you use chrome driver.
         # here you can download,
         # "http://chromedriver.chromium.org/downloads"
 
         # open driver
-        driver_path = './chromedriver'
-        self.driver = webdriver.Chrome(driver_path)
+        driver_path = '../chromedriver'
+        self.__driver = webdriver.Chrome(driver_path)
+        self.__get_screenshot_as_png = self.__driver.get_screenshot_as_png
 
         # window size
         window = (500, 420+123)
 
         # coodination of center of page
-        self.center_x = window[0]//2
-        self.center_y = 256
+        self.__center_x = window[0]//2
+        self.__center_y = 256
 
         # set window size
-        self.driver.set_window_size(*window)
+        self.__driver.set_window_size(*window)
 
         # move to target url
         target_url = 'http://typingx0.net/sushida/play.html'
-        self.driver.get(target_url)
+        self.__driver.get(target_url)
 
         target_xpath = '//*[@id="game"]/div'
-        self.webgl_element = self.driver.find_element_by_xpath(target_xpath)
-        actions = ActionChains(self.driver)
-        actions.move_to_element(self.webgl_element)
+        self.__webgl_element = self.__driver.find_element_by_xpath(target_xpath)
+        actions = ActionChains(self.__driver)
+        actions.move_to_element(self.__webgl_element)
         actions.perform()
 
         target_xpath = '/html/body'
-        self.body_element = self.driver.find_element_by_xpath(target_xpath)
+        self.__send_keys = self.__driver.find_element_by_xpath(target_xpath).send_keys
 
-    def _start_action(self):
+        return
+
+
+    def __start_action(self):
         # before start action
-        while True:
-            self.driver.save_screenshot(self.fname)
+        k = 0
+        while k == 0:
+            # save iostream image and clip an image
+            im = image_open(BytesIO(self.__get_screenshot_as_png())).convert('L')
 
-            # clip an image
-            im = Image.open(self.fname).convert('L')
-            k = im.getpixel((self.center_x, self.center_y))
-            if k!= 0:
-                break
+            k = im.getpixel((self.__center_x, self.__center_y))
 
         # click start action
-        actions = ActionChains(self.driver)
-        actions.move_to_element_with_offset(self.webgl_element, self.center_x, self.center_y).click().perform()
+        actions = ActionChains(self.__driver)
+        actions.move_to_element_with_offset(self.__webgl_element, self.__center_x, self.__center_y).click().perform()
         print("CLICKED START BUTTON")
 
-    def _course_action(self):
+        return
+
+
+    def __course_action(self):
         # before course action
-        while True:
-            # save iostream image
-            im_bin = self.driver.get_screenshot_as_png()
-            im_bin_stream = io.BytesIO(im_bin)
+        k = 0
+        while k != 255:
+            # save iostream image and clip an image
+            im = image_open(BytesIO(self.__get_screenshot_as_png())).convert('L')
 
-            # clip an image
-            im = Image.open(im_bin_stream).convert('L')
-
-            k = im.getpixel((self.center_x, self.center_y))
-            if k == 255:
-                break
+            k = im.getpixel((self.__center_x, self.__center_y))
 
         # click course action
-        actions = ActionChains(self.driver)
-        actions.move_by_offset(0, self.coord).click().perform()
+        actions = ActionChains(self.__driver)
+        actions.move_by_offset(0, self.__coord).click().perform()
         print("CLICKED COURSE BUTTON")
 
-    def _start_game(self):
+        return
+
+
+    def __start_game(self):
         # input SPACE for starting game
-        self.body_element.send_keys(" ")
+        self.__send_keys(" ")
 
         # before displaying question
-        while True:
-            # save iostream image
-            im_bin = self.driver.get_screenshot_as_png()
-            im_bin_stream = io.BytesIO(im_bin)
+        k = 0
+        while k != 255:
+            # save iostream image and clip an image
+            im = image_open(BytesIO(self.__get_screenshot_as_png())).convert('L')
 
-            # clip an image
-            im = Image.open(im_bin_stream).convert('L')
+            k = im.getpixel((self.__center_x+self.__xs[0], self.__y))
 
-            k = im.getpixel((self.center_x+self.xs[0], self.y))
-            if k == 255:
-                return
+        return
 
-    def _identify_cost_and_resave_im(self, im, xs):
 
-        for i in range(len(xs)):
-            k = im.getpixel((self.center_x+xs[i], self.y))
-            if k == 255:
+    def __identify_cost_and_resave_im(self, im):
+
+        xs = self.__xs
+        for x in xs:
+            if im.getpixel((self.__center_x+x, self.__y)) == 255:
 
                 # resave an image to bicolor: white or black, not gradation
-                im = im.crop((self.center_x-xs[i]+32, 230, self.center_x+xs[i]-32, 254))
-                for imx in range(im.width):
-                    for imy in range(im.height):
-                        k = im.getpixel((imx, imy))
-                        # if just middle gray and lighter which is font collor, it will black
-                        if k >= 128:
-                            im.putpixel((imx, imy), 0)
-                        # else which is background collor, it will white
-                        else:
-                            im.putpixel((imx, imy), 255)
+                im = asarray(im.crop((self.__center_x-x+32, 230, self.__center_x+x-32, 254)))
+                # when convering PIL image into numpy array, the array is immutable
+                im.flags.writeable = True
 
-                return self.costs[i], im
+                # if just middle gray and lighter which is font collor, it will black
+                # else which is background collor, it will white
+                # -1 of uint8 is 255. eg 136 >> 7 == 1, 1-1 == 0(black). 94 >> 7 == 0, 0-1 == -1 == 255(white)
+                return self.__costs[xs.index(x)], fromarray((im>>7)-1)
 
-    def _predict_text(self, im):
-
-        text = self.tool.image_to_string(im, lang='eng', builder=pyocr.builders.TextBuilder())
-
-        return re.sub("[^a-z,\-\!\?]", "", text)
 
     def auto_game(self):
         # initialize pre text
         pre_text = ""
-        predict_dishes = 0
 
-        self._open_chrome()
-        self._start_action()
-        self._course_action()
-        self._start_game()
+        self.__open_chrome()
+        self.__start_action()
+        self.__course_action()
+        self.__start_game()
 
-        while True:
-            # if you fail technical avoidance, break
-            try:
-                # save iostream image
-                im_bin = self.driver.get_screenshot_as_png()
-                im_bin_stream = io.BytesIO(im_bin)
+        send_keys = self.__send_keys
 
-                # clip an image
-                im = Image.open(im_bin_stream).convert('L')
-
-                # if finish the game, break
-                # identify twitter logo
-                k = im.getpixel((self.center_x+48, self.center_y+48))
-                if k == 132:
-                    break
+        # if you fail technical avoidance, break
+        try:
+            while True:
+                # save iostream image and clip an image
+                im = image_open(BytesIO(self.__get_screenshot_as_png())).convert('L')
 
                 # identify cost of dish
                 # and resave an image to bicolor: white or black, not gradation
-                cost, im = self._identify_cost_and_resave_im(im, self.xs)
-
-                # save file for corpus
-                # im.save("./corpus/__corpus_{:03}.png".format(predict_dishes))
+                cost, im = self.__identify_cost_and_resave_im(im)
 
                 # analyze text
-                text = self._predict_text(im)
+                text = resub("[^a-z,\-\!\?]", "", self.__image_to_string(im))
 
                 # send text
-                self.body_element.send_keys(text)
+                send_keys(text)
 
                 # print text on terminal
                 print(cost, "YEN DISH:", text.upper())
 
                 # if failing reading string, bruteforcing spell
                 if pre_text == text or text == "":
-                    self.body_element.send_keys("-,!?abcdefghijklmnopqrstuvwxyz")
+                    send_keys("-,!?abcdefghijklmnopqrstuvwxyz")
                     im.save("corpus/error_"+pre_text+"_.png")
                 else:
                     # save pre_text for failing image analysis
                     pre_text = text
-                    predict_dishes += 1
-                    if predict_dishes == self.max_dishes:
-                        break
 
-            # if you fail technical avoidance, break
-            except Exception:
-                break
+        # end the game
+        except TypeError:
+            print("======== END THE GAME ========")
+
+        # if you fail technical avoidance, break
+        except Exception:
+            print(format_exc())
+
+        return
+
 
     def __del__(self):
         # if input sth, exit
-        print("======== END THE GAME ========")
-        if self.input_sth:
+        if self.__input_sth:
             input("IF YOU EXIT, PLEASE INPUT STH")
 
-        # close driver
-        self.driver.close()
-        self.driver.quit()
 
-def main():
+
+if __name__ == "__main__":
     for i in range(1):
         atsushi = AutoSushida(level=0, input_sth=True)
         atsushi.auto_game()
         del atsushi
-
-if __name__ == "__main__":
-    main()
